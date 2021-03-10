ca2f81f4233516db4c3918ffde015c3c7ddaba0f
Aceki <aceki320@gmail.com>
2021-03-10 13:59:31 +0500

    Clean 'index.js' file

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 const { Core, Property, Type } = require('./lib');
 
-let c = new Core("bolt://0.0.0.0:7687", "neo4j", "niki3223");
-
-c.createGraph("MyGraphasdad");
-
-/*
-c.createClass("MyGraph", "c", [
-    new Property("A", Type.Number),
-    new Property("B", Type.Number),
-    new Property("C", Type.Number),
-    new Property("D", Type.String),
-    new Property("E", Type.String),
-    new Property("F", Type.String)
-])
-.then(() => console.log("ok"))
-.catch((err) => console.log("err"));*/
-
-/*
-c.deleteClass("MyGraph", "osas")
-.then(() => console.log("ok"))
-.catch((err) => console.log("err"));*/
-
 exports = {
     Core,
     Property,
