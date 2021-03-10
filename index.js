f5f03582e41c1bf97aafb5335d7a61818015dc2c
Aceki <aceki320@gmail.com>
2021-03-10 13:54:40 +0500

    Reorganize files

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const Core = require('./core');
-const Property = require('./property');
-const Type = require('./type');
+const { Core, Property, Type } = require('./lib');
+
+let c = new Core("bolt://0.0.0.0:7687", "neo4j", "niki3223");
+
+c.createGraph("MyGraphasdad");
+
+/*
+c.createClass("MyGraph", "c", [
+    new Property("A", Type.Number),
+    new Property("B", Type.Number),
+    new Property("C", Type.Number),
+    new Property("D", Type.String),
+    new Property("E", Type.String),
+    new Property("F", Type.String)
+])
+.then(() => console.log("ok"))
+.catch((err) => console.log("err"));*/
+
+/*
+c.deleteClass("MyGraph", "osas")
+.then(() => console.log("ok"))
+.catch((err) => console.log("err"));*/
 
 exports = {
     Core,
